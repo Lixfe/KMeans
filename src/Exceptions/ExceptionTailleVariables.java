69173294629ccd672e97cb8c8daa5b629f672f3a
RTRaineau <raphael.traineau@gmail.com>
2014-01-15 11:48:35 +0100

    ndistance euclidienne et exception pour tailles différentes

---
+++ src/Exceptions/ExceptionTailleVariables.java
@@ -0,0 +1,15 @@
+package Exceptions;
+
+public class ExceptionTailleVariables() extends Exception {
+
+	public int n,m;
+	
+	public ExceptionTailleVariable(int a, int b) {
+		n=a;
+		m=b;
+		
+	}
+	
+	
+	
+}
