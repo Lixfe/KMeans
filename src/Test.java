a5457e8af670e57eee2eec36f8338edfd967a215
RTRaineau <raphael.traineau@gmail.com>
2014-01-16 15:50:47 +0100

    modification du test et création classe kmeans

--- src/Test.java
+++ src/Test.java
@@ -22,13 +22,21 @@ public class Test {
 		clusterTest.calculerCentre();
 		Variable centre = new Variable(clusterTest.getCentre());
 		for (int i = 0; i < centre.getCoordonnees().size(); i++) {
-			System.out.println("Coordonn�e x"+i+" du centre : "+centre.getCoordonnees().get(i));
+			System.out.println("Coordonnee x"+i+" du centre : "+centre.getCoordonnees().get(i));
 		}
 		
 		//test de la distance euclidienne
 		Similarite sim = new DistanceEuclidienne()  ;
 		double d = sim.similarite(v1, v2); 
 		System.out.println("la distance euclidienne entre v1 et v2 est "+d);
+		
+		//test de la création d'un clustering
+		ArrayList<Variable> listeVariable = new ArrayList<Variable>();
+		listeVariable.add(v1);
+		listeVariable.add(v2);
+		
+		Clustering solutionClustering = new Clustering(listeVariable, 1);
+		solutionClustering.reallocation(sim);
 		
 		
 	}
