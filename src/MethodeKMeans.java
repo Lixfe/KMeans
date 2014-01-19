8fd3393f9225509d74a4b8de63364ceacdfbf39d
unknown <felix.leo.lecuyer@gmail.com>
2014-01-19 14:03:47 +0100

    Debut de main

---
+++ src/MethodeKMeans.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Classe principale du programme
+ * Contient le jeu de données considéré ainsi qu'une méthode  pour lire le jeu de données à partir d'un fichier
+ * externe
+ * Classe qui contient la méthode main
+ * @author Felix
+ *
+ */
+public class MethodeKMeans {
+
+	protected ArrayList<Variable> jeuDonnees;
+	
+	public void parse(String nomFichier){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		try{
+			//Lecture du nom de ficheir contenant les données sources
+			System.out.println("Nom du fichier contenant les données sources");
+			String fichier = new String();
+			Scanner sc = new Scanner();
+			fichier = sc.next();
+			
+			 
+		}
+		catch(IOException e){
+			System.out.println("Erreur lors de l'entrée du nom de fichier");
+		}
+
+	}
+
+}
