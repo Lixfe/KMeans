06aecfe473144c19af296e6a5a70f1670fa6b56f
RTRaineau <raphael.traineau@gmail.com>
2014-01-30 23:47:43 +0100

    ecriture classe methode kmeans et main

--- src/MethodeKMeans.java
+++ src/MethodeKMeans.java
@@ -1,7 +1,12 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
+
+import Exceptions.ExceptionTailleVariables;
+
+import com.sun.org.apache.xml.internal.serializer.ToUnknownStream;
 
 /**
  * Classe principale du programme
@@ -15,28 +20,35 @@ public class MethodeKMeans {
 
 	protected ArrayList<Variable> jeuDonnees;
 	
-	public void parse(String nomFichier){
 		
-	}
-	
 	
 	public static void main(String[] args) {
 		
 		try{
-			//Lecture du nom de ficheir contenant les données sources
-			System.out.println("Nom du fichier contenant les données sources");
-			String adresseFichier = new String();
+			//lecture d'un fichier
+			LectureFichier lf = new LectureFichier("C:\\donnees-kmeans.txt");
 			
-			Scanner sc = new Scanner(adresseFichier);
+			//on place les données lues dans le fichier dans une liste de variables (le 2 correspond à la dimension des variables)
+			ArrayList<Variable> donneesFichier = lf.toIntArray(2);
 			
-			adresseFichier = sc.next();
+			//on initialise le probleme avec une distance
+			Similarite sim = new DistanceEuclidienne()  ;
+			KMeans probleme = new KMeans(2, sim);
 			
+			//on lance l'algorithme du kmeans
+			probleme.run(donneesFichier);
 			
-			sc.close();
-			 
+	
+			
+		} catch (ExceptionTailleVariables e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		catch(IOException e){
+		/*catch(IOException e){
 			System.out.println("Erreur lors de l'entrée du nom de fichier");
+		}*/
+		finally {
+			System.out.println("fini !");
 		}
 
 	}
--- src/Test.java
+++ src/Test.java
@@ -12,8 +12,8 @@ public class Test {
 		coordV1.add(0.);
 		Variable v1 = new Variable(coordV1); 
 		ArrayList<Double> coordV2 = new ArrayList<Double>();
-		coordV2.add(2.);
-		coordV2.add(2.);
+		coordV2.add(0.);
+		coordV2.add(1.);
 		Variable v2 = new Variable(coordV2);
 		
 		Cluster clusterTest = new Cluster(v1);
@@ -55,22 +55,34 @@ public class Test {
 		probleme.run(listeVariable);
 		*/
 		
-		//deuxieme test
-		KMeans probleme2 = new KMeans(1, sim);
+		/*deuxieme test
+		KMeans probleme2 = new KMeans(2, sim);
 		
 		ArrayList<Double> coordV3 = new ArrayList<Double>();
-		coordV3.add(4.);
-		coordV3.add(4.);
+		coordV3.add(0.);
+		coordV3.add(1.5);
 		Variable v3 = new Variable(coordV3);
 		listeVariable.add(v3);
 		
 		ArrayList<Double> coordV4 = new ArrayList<Double>();
-		coordV4.add(5.);
-		coordV4.add(5.);
+		coordV4.add(0.);
+		coordV4.add(2.5);
 		Variable v4 = new Variable(coordV4);
 		listeVariable.add(v4);
 		
-		probleme2.run(listeVariable);
+		probleme2.run(listeVariable);*/
+		
+		//test de la lecture fichier
+		LectureFichier lf = new LectureFichier("C:\\donnees-kmeans1.txt");
+		ArrayList<Variable> rep = lf.toIntArray(2);
+		for (int i = 0; i < rep.size(); i++) {
+			System.out.print("Coordonnees de la varible "+i+" : ");
+			for (int j=0 ; j<rep.get(i).coordonnees.size() ; j++){
+				System.out.print(rep.get(i).coordonnees.get(j)+" ; ");
+			}
+			
+		}
+		
 	}
 
 }
